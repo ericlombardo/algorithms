ca385e889a1ff4e790b6bf8dbc8226694d2828d0
Eric <eric.m.lombardo@gmail.com>
2021-10-11 12:44:22 -0400

    Setup and describe problem for removeOneElement

---
+++ Leet Code/removeOneElementToMakeStrictlyIncreasing.js
@@ -0,0 +1,11 @@
+
+// @param {number[]} nums
+// @return {boolean}
+
+// Have array of integers.
+// Should increase by each element, no repeat numbers
+// Return true if we can achieve this by removing 1 element, false if not
+ var canBeIncreasing = function(nums) {
+
+
+};
