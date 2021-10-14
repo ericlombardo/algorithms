774a4a957047ad05dd3745a5f5fc268b5898910d
Eric <eric.m.lombardo@gmail.com>
2021-10-14 19:57:53 -0400

    Start working through the problem
    Add logic for iterating through nums array and checking if value is less
    than the next.
    
    Running into issue when value is strictly increasing and needs to skip
    to the next value but compare that to the previous value. Will try to
    compare values, and if it is less than will add to an array. This will
    allow me to check the previous value by recalling the last value in the
    confirmed array.

--- Leet Code/removeOneElementToMakeStrictlyIncreasing.js
+++ Leet Code/removeOneElementToMakeStrictlyIncreasing.js
@@ -5,7 +5,31 @@
 // Have array of integers.
 // Should increase by each element, no repeat numbers
 // Return true if we can achieve this by removing 1 element, false if not
- var canBeIncreasing = function(nums) {
+var canBeIncreasing = function(nums) {
 
+  let counter = 0
+  let checkNum = 0
+  let newNums = nums
+  for (let i = 0; i < nums.length; i++) {
+      if (nums[i] >= nums[i + 1]) {
+        counter++
+        newNums.splice(i, 1)
+    }
+  }
+  console.log(counter)
+  console.log(counter < 2 ? true : false)
 
 };
+
+num = [1,4,10,3,6]
+// nums = [
+  // [1,2,3] // true
+  // [1,1,1], // false
+  // [1,2,10, 3,4], // true
+  // [1,2,7,3,4,8,5], //false
+  // [2,3,1,2] // false
+// ]
+
+// for (const num of nums) {
+  canBeIncreasing(num)
+// }
