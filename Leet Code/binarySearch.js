523e5e849f4862e4eca8660bea87ff991198ebb0
Eric <eric.m.lombardo@gmail.com>
2021-10-24 12:31:15 -0400

    Set up problem 704 and write out sudo code

---
+++ Leet Code/binarySearch.js
@@ -0,0 +1,16 @@
+// Given an array of integers nums which is sorted in ascending order,
+// and an integer target, write a function to search target in nums.
+// If target exists, then return its index. Otherwise, return -1.
+
+// Notes: 
+    // go to middle of nums array
+    // return index if match to target
+    // if less than target repeat with higher half
+    // if more than target repeat with lower half
+    
+    // create two variables and put half in each (round up)
+    // check last element of first array and use recursion passing in appropriate array
+    
+var search = function(nums, target) {
+  
+}
