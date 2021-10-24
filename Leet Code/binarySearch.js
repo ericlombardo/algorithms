4e6c0b9e8f98a17459dfe557c659d349f2a31cad
Eric <eric.m.lombardo@gmail.com>
2021-10-24 13:58:09 -0400

    Initial solution to binary search problem

--- Leet Code/binarySearch.js
+++ Leet Code/binarySearch.js
@@ -10,7 +10,27 @@
     
     // create two variables and put half in each (round up)
     // check last element of first array and use recursion passing in appropriate array
-    
-var search = function(nums, target) {
+    var search = function(nums, target) {
   
-}
+      const findNumber = (array) => {
+      
+          if (array.length === 1 && array[0] === target) {
+              return nums.indexOf(array[0])
+          } else if (array.length === 1 && array[0] !== target) {
+              return -1
+          }
+          let left = array.slice(0, (array.length / 2))
+          let right = array.slice(left.length)
+        
+          let middle = left[left.length - 1 ]
+
+          if (middle === target) {
+              return nums.indexOf(middle)
+          } else if (middle > target) {
+              return findNumber(left)
+          } else if (middle < target) {
+              return findNumber(right)
+          }
+      }
+      return findNumber(nums)
+  }
