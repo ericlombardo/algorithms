edae9583dacd7fe9f029516481bb530fe869752d
Eric <eric.m.lombardo@gmail.com>
2021-11-04 15:36:37 -0400

    Refactor for more time efficient solution

--- Master the Coding Interview/twoArraysWithSameElement.js
+++ Master the Coding Interview/twoArraysWithSameElement.js
@@ -6,15 +6,24 @@ array4 = ['z', 'y', 'x']
 //return false
 
 const checkArrays = (array1, array2) => {
+  let map = {}
   for (let i = 0; i < array1.length; i++) {
-    for (let x = 0; x < array2.length; x++) {
-      if (array1[i] === array2[x]) console.log('true')
+    if (!map[array1[i]]) {
+      map[array1[i]] = true 
     }
+  }
+  for (let j = 0; j < array2.length; j++) {
+    if (map[array2[j]]) return true
   }
   return false
 }
 checkArrays(array1, array2)
 
+// brute force way below:
 
-
- 
+ // for (let i = 0; i < array1.length; i++) {
+  //   for (let x = 0; x < array2.length; x++) {
+  //     if (array1[i] === array2[x]) console.log('true')
+  //   }
+  // }
+  // console.log('false')
