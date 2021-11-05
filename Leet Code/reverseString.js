d11d0ad727625fa8dde8d7cff4a8f17f80df525c
Eric <eric.m.lombardo@gmail.com>
2021-11-05 15:48:11 -0400

    Setup for reverseString problem with notes

---
+++ Leet Code/reverseString.js
@@ -0,0 +1,13 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+ var reverseString = function(s) {
+  return function() {
+      // Don't create new array
+      // Need to swap first and last
+      // Move in one
+      // Swap them
+      // Move in one until the middle
+  }
+};
