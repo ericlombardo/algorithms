3bcc499366cef4cce5bdba95a8eedef635ea338d
Eric <eric.m.lombardo@gmail.com>
2021-11-08 16:48:06 -0500

    Reformat binarySearch.js to try different solution

--- Leet Code/binarySearch.js
+++ Leet Code/binarySearch.js
@@ -2,35 +2,31 @@
 // and an integer target, write a function to search target in nums.
 // If target exists, then return its index. Otherwise, return -1.
 
-// Notes: 
-    // go to middle of nums array
-    // return index if match to target
-    // if less than target repeat with higher half
-    // if more than target repeat with lower half
-    
-    // create two variables and put half in each (round up)
-    // check last element of first array and use recursion passing in appropriate array
-    var search = function(nums, target) {
-  
-      const findNumber = (array) => {
-      
-          if (array.length === 1 && array[0] === target) {
-              return nums.indexOf(array[0])
-          } else if (array.length === 1 && array[0] !== target) {
-              return -1
-          }
-          let left = array.slice(0, (array.length / 2))
-          let right = array.slice(left.length)
+    var search = function(nums, target) {   
+        // start at a point
+        // check if point === target
+            // return index of point
+        // check if point < target
+            // pick another point between point and end of nums
+            // check again
+        // check if point > target
+            // pick another point between point and beginning of nums
+            // check again
+        let pointer = pickMiddle(0, nums.length - 1)
         
-          let middle = left[left.length - 1 ]
-
-          if (middle === target) {
-              return nums.indexOf(middle)
-          } else if (middle > target) {
-              return findNumber(left)
-          } else if (middle < target) {
-              return findNumber(right)
-          }
-      }
-      return findNumber(nums)
-  }
+        const pickMiddle = (frontIndex, backIndex) => {
+            return Math.round(backIndex / frontIndex)
+        }
+        
+        const checkForMatch = (pointer) => {
+            
+            if (nums[pointer] === target) { // check if pointer === target
+                return pointer
+            } else if (nums[pointer] < target) {    // check if pointer < target
+              pointer = pickMiddle(pointer, nums.length - 1)
+              checkForMatch(pointer)
+            } else if (nums[pointer] > target) {    // check if pointer > target
+                pointer = pickMiddle(0, pointer)
+            }
+        }
+    }
