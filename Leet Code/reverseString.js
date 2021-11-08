5ed5eea05cc80d352f30cb1cb422e7529981d09a
Eric <eric.m.lombardo@gmail.com>
2021-11-08 15:58:02 -0500

    Possible solution using recursion
    * Returns reversed string
    * Needs to return an array instead of string
    
    Problem:
    When iterating through function, I need to find point to create array
    from returned string.

--- Leet Code/reverseString.js
+++ Leet Code/reverseString.js
@@ -3,24 +3,35 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 
-/**
- * s = 'eric'
- * s = 'cire'
- *  */ 
-
 var reverseString = function(s) {
-
-  if (!s || s.length < 2 || typeof s !== 'string') {
-    return 'No string to reverse, try again'
+  if (s.length === 0) { // set the check to avoid infinite loop
+    return ""
   }
 
-  let newArray = []
+  // take first character and then run reverseString again removing first character
+  // continue this until you 
+   return reverseString(s.slice(1)) + s[0]
 
-  for (let i = s.length - 1; i >= 0; i--) {
-    newArray.push(s[i])
-  }
-  return newArray.join('')
+};
+console.log(reverseString(['h', 'e', 'l', 'l', 'o']))
+
+// found suraj Sharma youtube video to help explain the concepts of recursion
+// https://www.youtube.com/watch?v=AgMq_wzwAXM
+
+
+// Original Way
+// var reverseString = function(s) {
+
+//   if (!s || s.length < 2 || typeof s !== 'string') {
+//     return 'No string to reverse, try again'
+//   }
+
+//   let newArray = []
+
+//   for (let i = s.length - 1; i >= 0; i--) {
+//     newArray.push(s[i])
+//   }
+//   return newArray.join('')
 
  
-};
-reverseString('eric')
+// };
