b788ed3cbff2f3f694d6e6c1f373f26052e93d78
Eric <eric.m.lombardo@gmail.com>
2021-11-10 12:01:49 -0500

    New solution to manually sort joined array

--- Master the Coding Interview/mergeSortedArrays.js
+++ Master the Coding Interview/mergeSortedArrays.js
@@ -1,9 +1,17 @@
 const mergeSortedArrays = (arr1, arr2) => {
 
-  return arr1.concat(arr2).sort((a, b) => a - b )
+  let mergedArray = [...arr1, ...arr2] 
+  let temp 
+  
+  for (let i = 0; i < mergedArray.length; i++) {
+    if (mergedArray[i] > mergedArray[i + 1]) {
+      temp = mergedArray[i]
+      mergedArray[i] = mergedArray[i + 1]
+      mergedArray[i + 1] = temp
+    } 
+  }
+  return mergedArray
 }
-
-
 
 console.log(mergeSortedArrays([0,3,4,31], [4,6,30]))
 // [ 0, 3, 4, 4, 6, 30, 31 ]
