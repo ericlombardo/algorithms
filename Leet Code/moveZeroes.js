313ede6ec0cd55dd13db1eed480d5ceef4f6a91b
Eric <eric.m.lombardo@gmail.com>
2021-11-11 19:18:59 -0500

    Fix iteration count when zero is removed

--- Leet Code/moveZeroes.js
+++ Leet Code/moveZeroes.js
@@ -1,16 +1,24 @@
 var moveZeroes = function(nums) {
     
   let start = performance.now()
-  // slide all other values down
+  
+  let count = 0
+  // abstract to function (findAndRemoveZeroes)
   for (let i = 0; i < nums.length; i++) { // iterate through array
       if (nums[i] === 0) {    // if val = 0 add to end
           nums.splice(i,1)    // remove that value
-          nums.push(0)        // add it to the end
+          count++
+          i--
       }
   }
+  // abstract to function (addZeroesToEnd)
+  for (let i = 0; i < count; i++) {
+    nums.push(0)
+  }
+
   let end = performance.now()
   console.log(`${end - start} milliseconds to run`)
   return nums
 };
 
-console.log(moveZeroes([0,1,0,3,12]))
+console.log(moveZeroes([0,0,1]))
