7c53c7ab2d6d3182de7fc9243f288109f9eb2697
Eric <eric.m.lombardo@gmail.com>
2021-11-16 18:13:52 -0500

    New solution that makes it O(n)
    Iterates through, checks if value already in object, if so returns
    true, if not pushes that value into the object

--- Leet Code/containsDuplicate.js
+++ Leet Code/containsDuplicate.js
@@ -1,19 +1,31 @@
 // return true or false base one if array has any repeats
 
 var containsDuplicate = function(nums) {
-  
+  let duplicateNums = []
   // check if array and if array is empty
   if (nums.length === 0 || typeof(nums) !== 'object') {
     return 'try valid array'
   }
-   for (let i = 0; i < nums.length; i++) {
-     for (let j = i + 1; j < nums.length; j++) {
-       if (nums[i] === nums[j]) {
-         return true
-       }
-     }
-   }
-   return false
+   
+  for (let i = 0; i < nums.length; i++) {
+    if (duplicateNums[nums[i]]) {
+      return true
+    } else {
+      duplicateNums[nums[i]] = true
+    }
+  }
+  return false
 };
 
-console.log(containsDuplicate([1,1,1,3,3,4,5]))
+console.log(containsDuplicate([1,2,3,4,1,1,2]))
+
+
+// nested loop solution
+// for (let i = 0; i < nums.length; i++) {
+//   for (let j = i + 1; j < nums.length; j++) {
+//     if (nums[i] === nums[j]) {
+//       return true
+//     }
+//   }
+// }
+// return false
