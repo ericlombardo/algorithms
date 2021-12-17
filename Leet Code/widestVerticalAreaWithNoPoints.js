b1f34ebb0f63b19c8dfe962ae8e80d833fd3378e
Eric <eric.m.lombardo@gmail.com>
2021-12-17 14:11:47 -0500

    Setup maxWidthOfVerticalArea and thoughts to solve

---
+++ Leet Code/widestVerticalAreaWithNoPoints.js
@@ -0,0 +1,8 @@
+// Return the widest vertical area between two points such that no points are inside the area.
+
+ var maxWidthOfVerticalArea = function(points) {
+  // take the x variables
+  // sort them 
+  // iterate through, subtract high from low, then push highest value
+
+};
