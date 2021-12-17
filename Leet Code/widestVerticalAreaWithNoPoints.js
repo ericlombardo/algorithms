3fefd75abff89272f8a7a46103eb3dfaa360d1a5
Eric <eric.m.lombardo@gmail.com>
2021-12-17 14:14:13 -0500

    Solution for maxWidthOfVeritcalArea.js
    * Iterate through points to collect x coordinate
    * Sort x coordinates to remove issue of points within width
    * Iterate through x coordinates and calculate width to get set and
      return widestVerticalArea variable

--- Leet Code/widestVerticalAreaWithNoPoints.js
+++ Leet Code/widestVerticalAreaWithNoPoints.js
@@ -1,8 +1,24 @@
 // Return the widest vertical area between two points such that no points are inside the area.
 
  var maxWidthOfVerticalArea = function(points) {
-  // take the x variables
-  // sort them 
+   
+   let xPoints = []
+   let widestVerticalArea = 0
+   
+   // take the x variables from each point
+   for (const pointsSet of points) {
+     xPoints.push(pointsSet[0])
+    }
+    // sort them to eliminate having points within 
+    xPoints.sort((a, b) => a - b)
+    
+    
   // iterate through, subtract high from low, then push highest value
+  for ( let i = 0; i < xPoints.length; i++) {
+      if ((xPoints[i + 1] - xPoints[i]) > widestVerticalArea) {
+          widestVerticalArea = (xPoints[i + 1] - xPoints[i])
+      }
+  }
 
+  return widestVerticalArea
 };
