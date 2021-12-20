d8f196f1f3844f190e191a07cbbe8096eaf99a9e
Eric <eric.m.lombardo@gmail.com>
2021-12-19 20:27:59 -0500

    Solution for reverseString.js using recursion

---
+++ Online Sources/reverseString.js
@@ -0,0 +1,20 @@
+var reverseString = function(s) {
+  let left = 0
+  let right = s.length - 1
+
+  const recur = (str, left, right) => {
+    let temp
+    if (left < right) {
+      temp = str[right]
+      str[right] = str[left]
+      str[left] = temp
+      return recur(str, left + 1, right - 1)
+    }
+    return str
+  }
+  return recur(s, left, right)
+};
+
+
+// Tom's YouTube video helped show me where I was going wrong with my temp assignment
+// https://www.youtube.com/watch?v=AgMq_wzwAXM
