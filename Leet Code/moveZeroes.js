511c17474a864a228d35e4cd6afd6dbfc2f1bb9d
Eric <eric.m.lombardo@gmail.com>
2022-01-24 11:25:02 -0500

    Add alternate solution to 282. Move Zeroes
    Use fast and slow pointer to remove zeros and set count
    Use count to switch last numbers to 0

--- Leet Code/moveZeroes.js
+++ Leet Code/moveZeroes.js
@@ -1,24 +1,55 @@
-var moveZeroes = function(nums) {
-    
-  let start = performance.now()
-  
-  let count = 0
-  // abstract to function (findAndRemoveZeroes)
-  for (let i = 0; i < nums.length; i++) { // iterate through array
-      if (nums[i] === 0) {    // if val = 0 add to end
-          nums.splice(i,1)    // remove that value
-          count++
-          i--
-      }
-  }
-  // abstract to function (addZeroesToEnd)
-  for (let i = 0; i < count; i++) {
-    nums.push(0)
-  }
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
 
-  let end = performance.now()
-  console.log(`${end - start} milliseconds to run`)
-  return nums
+ var moveZeroes = function(nums) {
+  let count = 0
+  
+// abstract to function (findAndRemoveZeroes)
+for (let i = 0; i < nums.length; i++) { // iterate through array
+    if (nums[i] === 0) {    // if val = 0 add to end
+        nums.splice(i,1)    // remove that value
+        count++
+        i--
+    }
+}
+// abstract to function (addZeroesToEnd)
+for (let i = 0; i < count; i++) {
+  nums.push(0)
+}
+
+return nums
+ 
 };
 
-console.log(moveZeroes([0,0,1]))
+/* 
+Alternate Solution:
+
+var moveZeroes = function(nums) {
+  let slow = 0
+  let fast = 0
+  let count = 0
+  
+  while (fast < nums.length) {
+      if (nums[fast] !== 0) {
+          nums[slow] = nums[fast]
+          slow++
+      } else {
+          count++
+      }
+      fast++
+  }
+  
+
+  let last = nums.length - count
+  
+  while ( last < nums.length) {
+      
+      nums[last] = 0
+      last++
+  }
+}
+
+
+*/
