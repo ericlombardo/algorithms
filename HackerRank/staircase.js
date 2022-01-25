48672cbf01fd7ce6c6a30a3ac5820aba64154d31
Eric <eric.m.lombardo@gmail.com>
2022-01-25 12:27:24 -0500

    Create staircase using spaces and hashes

---
+++ HackerRank/staircase.js
@@ -0,0 +1,67 @@
+function staircase(n) {
+  let spaceCount = n - 1
+  let hashCount = 1
+  
+  while (spaceCount >= 0) {
+    console.log(' '.repeat(spaceCount) + '#'.repeat(hashCount))
+    spaceCount--
+    hashCount++  
+  }
+}
+
+console.log(staircase(5))
+
+/* 
+
+Staircase detail
+
+This is a staircase of size :
+
+   #
+  ##
+ ###
+####
+Its base and height are both equal to . It is drawn using # symbols and spaces. The last line is not preceded by any spaces.
+
+Write a program that prints a staircase of size .
+
+Function Description
+
+Complete the staircase function in the editor below.
+
+staircase has the following parameter(s):
+
+int n: an integer
+Print
+
+Print a staircase as described above.
+
+Input Format
+
+A single integer, , denoting the size of the staircase.
+
+Constraints
+
+ .
+
+Output Format
+
+Print a staircase of size  using # symbols and spaces.
+
+Note: The last line must have  spaces in it.
+
+Sample Input
+
+6 
+Sample Output
+
+     #
+    ##
+   ###
+  ####
+ #####
+######
+Explanation
+
+The staircase is right-aligned, composed of # symbols and spaces, and has a height and width of .
+*/
